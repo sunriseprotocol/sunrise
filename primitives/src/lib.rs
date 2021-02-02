72e3b2c183e9dd3cf1153dd1f7a3868dda106ff3
gdevSRS <gdevSRS>
2021-02-02 16:14:35 -0500

    added makefile and primitives

---
+++ primitives/src/lib.rs
@@ -0,0 +1,29 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use codec::{Decode, Encode};
+
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+use sp_runtime::{RuntimeDebug};
+
+pub type Balance = u128;
+
+pub type EvmAddress = sp_core::H160;
+
+
+#[derive(Encode, Decode, Eq, PartialEq, Copy, Clone, RuntimeDebug , PartialOrd, Ord)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+pub enum CurrencyId {
+	Native(TokenSymbol),
+	ERC20(EvmAddress),
+}
+
+#[derive(Encode, Decode, Eq, PartialEq, Copy, Clone, RuntimeDebug, PartialOrd, Ord)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+pub enum TokenSymbol {
+	SRS = 0,
+	DOT = 1,
+	KSM = 2,
+	BTC = 3,
+
+}
